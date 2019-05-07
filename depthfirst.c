fe51bab11880dda831bda717932bc0d9993e8078
Zhimin Liang <liang.jimmy12@gmail.com>
2019-05-06 19:49:31 -0700

    skeleton
    code doesn't work but can serve as a skeleton template to see what functions need work

--- depthfirst.c
+++ depthfirst.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#define N 5
 
 //Linked list code from https://www.hackerearth.com/practice/data-structures/linked-list/singly-linked-list/tutorial/
 struct LinkedList{
@@ -54,6 +55,7 @@ nodeList createNodeList(){
     temp->next = NULL;// make next point to NULL
 	node tempNode;
 	tempNode=createNode();
+	tempNode->data=-1;
 	temp->data=tempNode;
     return temp;//return the new node
 }
@@ -77,7 +79,32 @@ nodeList push(nodeList head, node path){
     return head;
 }
 
-void printStack(nodeList p) {
+//if head is only 1 item does nothing
+node pop(nodeList head){
+    nodeList temp,p;// declare two nodes temp and p
+    temp = createNodeList();//createNode will return a new node with data = value and next pointing to NULL.
+    if(head == NULL){
+        head = temp;     //when linked list is empty
+	printf("headnull\n");
+	//printStack(head);
+    }
+    else{
+        p  = head;//assign head to p 
+        while(p->next != NULL){
+		temp=p;
+            p = p->next;//traverse the list until p is the last node.The last node always points to NULL.
+        }
+        node tempNode=p->data;
+	temp->next=NULL;
+	
+    }
+    return tempNode;
+}
+
+
+void printStack(nodeList path) {
+	nodelist p;
+	p=path;
 	p=p->next;
 	while(p != NULL) {
 	printNode(p->data);
@@ -86,11 +113,38 @@ void printStack(nodeList p) {
 	}
 }
 
+int pathCost(node path) {
+	nodelist p;
+	p=path;
+	p=p->next;
+	int sum=0;
+	int tempsum;
+	while(p != NULL) {
+		tempsum=distance[p->data][(p->next)->data];
+		sum+=tempsum;
+		p=p->next;
+	}
+	return sum;
+}
+
+//makes sure not going to the city you are already at
+boolean feasible(node path, int city) {
+	node p=path;
+	while(p->next != NULL) {
+		p=p->next;
+	}
+	int last = p->data;
+	if(data != city)
+		return true;
+	else
+		return false;
+}
+
 int main(int argc, char *argv[])
 {
-	const int N=5;
+	//const int N=5;
 	const int start=0;
-	const int distance[5][5]={{-1, 3, 5, 2, 8}, {3.1, -1, 9, 4.2, 6.4}, {5.1, 9.1, -1, 6.2, 6.3}, {1.3, 1.5, 7.5, 9.3, 11}};	//can't replace with N?
+	static const int distance[N][N]={{-1, 3, 5, 2, 8}, {3.1, -1, 9, 4.2, 6.4}, {5.1, 9.1, -1, 6.2, 6.3}, {1.3, 1.5, 7.5, 9.3, 11}};	//can't replace with N?
 	nodeList stack = createNodeList();
 /*
 	stack=NULL;
@@ -107,20 +161,44 @@ int main(int argc, char *argv[])
 	for(i=0; i<N; i++) {	//figure out how to check initialization condition
 		if(i != start) {
 		temp = createNode();
-		temp->data = distance[start][i];
+		//temp->data = distance[start][i];
+		temp->data=i;
 		push(stack, temp);
 		}
 	}
 	printStack(stack);
-/*
 
-  #pragma omp parallel for shared(N, start, distance, stack) private(minDist, bestPath)
+
+	boolean done=false;
+	int popdone=0;
+  #pragma omp parallel shared(start, distance, stack, done) private(minDist, bestPath)
 	{
-		int i;
-		for (i=0; i<N; i++)
-		{
+		while(!done) {
+
+		node tempPath
+		#pragma omp critical {
+			tempPath=pop(stack);
+			popdone+=1}
+		if(tempPath->data)==-1)
+			if(popdone==1)
+				done=true;
+			else
+				popdone-=1;
+		else {
+			int i;
+			for (i=0; i<N; i++)
+			{
+				if(feasible(tempPath)) {
+				newPath=addNode(tempPath, i)
+					#pragma omp critical {
+						push(stack, newPath);}
+				}
+			}
+			popdone-=1;
+		}
 
 		}
 	}
-*/
+}
+
 }
